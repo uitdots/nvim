f79f5b13442e72d1e59bd44f35bb8aa0e1a245ab
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-19 19:20:13 +0700

    feat(mason): reload file to attach new lsp

--- lua/plugins/lsp/core/mason.lua
+++ lua/plugins/lsp/core/mason.lua
@@ -39,4 +39,18 @@ return {
       "github:mason-org/mason-registry",
     },
   },
+  -- https://www.lazyvim.org/plugins/lsp#masonnvim-1
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require("mason-registry")
+    mr:on("package:install:success", function()
+      vim.defer_fn(function()
+        -- trigger FileType event to possibly load this newly installed LSP server
+        require("lazy.core.handler.event").trigger({
+          event = "FileType",
+          buf = vim.api.nvim_get_current_buf(),
+        })
+      end, 100)
+    end)
+  end,
 }
