f22fa06f7533b4dbd953c013f45d9d6fc214e927
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-20 15:40:19 +0700

    chore(nvim-notify): add background transparent for nvim-notify always

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -2,25 +2,17 @@
 return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
-  opts = function(_, opts)
-    opts = {
-      level = 2,
-      minimum_width = 50,
-      render = "default",
-      stages = "fade_in_slide_out",
-      timeout = 3000,
-      top_down = false,
-    }
-
-    if require("nvconfig").base46.transparency then
-      opts.background_colour = "#000000"
-    end
-
-    return opts
-  end,
+  opts = {
+    level = 2,
+    minimum_width = 50,
+    render = "default",
+    stages = "fade_in_slide_out",
+    timeout = 3000,
+    top_down = false,
+    background_colour = "#000000",
+  },
   config = function(_, opts)
     require("notify").setup(opts)
-
     vim.notify = require("notify")
   end,
 }
