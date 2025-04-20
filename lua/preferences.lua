95d7e66c682b164753e61eb1225ed61f9cb46c5c
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-20 17:29:45 +0700

    chore(preferences): more clear for lsp customisation

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -1,19 +1,20 @@
 local g = vim.g
 local M = {}
 
--- NOTE: Add line by line the lsp you want to enable manually here (Install yourself, or "mason lspconfig" doesn't provide it yet, but neovim must support that)
+-- NOTE: Add line by line the lsp you want to enable manually here (Install yourself, or "mason lspconfig" doesn't provide it yet, but neovim must support that) (new API)
 -- Name taken from https://github.com/neovim/nvim-lspconfig/tree/master/lsp
 M.lsps = {
   "postgres_ls",
 }
 
 -- NOTE: For old lsp configurations (old nvim lsp way), and you want to enable manully
--- For those LSP listed here: https://github.com/neovim/nvim-lspconfig/issues/3705
+-- For those LSP listed here with uncheck: https://github.com/neovim/nvim-lspconfig/issues/3705
 -- This option will be removed in the future when nvim-lspconfig support all lsp in new way
 M.old_lsps = {}
 
 -- NOTE: Yes ... Need mason-lspconfig support for this
 -- We don't support customise those old lsp way because they will be support in the new way soon :P
+-- Will remove this in the future
 M.old_lsps_mason = {
   "eslint",
 }
