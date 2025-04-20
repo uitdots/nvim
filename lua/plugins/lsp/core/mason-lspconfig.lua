d10752b3e84254b9dbd96d7c016567c555a7e8d5
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-20 17:27:48 +0700

    refactor(preferences): easier to declare old lsp

--- lua/plugins/lsp/core/mason-lspconfig.lua
+++ lua/plugins/lsp/core/mason-lspconfig.lua
@@ -2,17 +2,26 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   event = "VeryLazy",
-  opts = {
-    handlers = {
+  -- NOTE: Change this to normal table when lsp is fully supported
+  opts = function(_, opts)
+    local server_names = require("preferences").old_lsps_mason
+    local old_lspconfig = require("configs.lspconfig").old_lspconfig
+
+    opts = opts or {}
+    opts.handlers = {
       function(server_name)
         vim.lsp.enable(server_name)
       end,
-      ["eslint"] = function()
-        require("configs.lspconfig").old_lspconfig("eslint")
-      end,
+      ["eslint"] = function() end,
       ["ts_ls"] = function() end,
       ["rust_analyzer"] = function() end,
       ["jdtls"] = function() end,
-    },
-  },
+    }
+
+    for _, server_name in pairs(server_names) do
+      opts.handlers[server_name] = function()
+        old_lspconfig(server_name)
+      end
+    end
+  end,
 }
--- lua/preferences.lua
+++ lua/preferences.lua
@@ -7,6 +7,17 @@ M.lsps = {
   "postgres_ls",
 }
 
+-- NOTE: For old lsp configurations (old nvim lsp way), and you want to enable manully
+-- For those LSP listed here: https://github.com/neovim/nvim-lspconfig/issues/3705
+-- This option will be removed in the future when nvim-lspconfig support all lsp in new way
+M.old_lsps = {}
+
+-- NOTE: Yes ... Need mason-lspconfig support for this
+-- We don't support customise those old lsp way because they will be support in the new way soon :P
+M.old_lsps_mason = {
+  "eslint",
+}
+
 -- NOTE: Where you put our custom configurations
 function M.setup()
   g.mapleader = " "
