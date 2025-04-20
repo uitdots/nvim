49a2c6e0f51678cd240cc3ab2b50b290d603d1f7
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-20 13:16:53 +0700

    fix(plugins): `filetype` not loaded

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,7 @@ return {
   { import = "plugins.debug" },
   { import = "plugins.editor" },
   { import = "plugins.file" },
+  { import = "plugins.filetype" },
   { import = "plugins.lsp" },
   { import = "plugins.neccessaries" },
   { import = "plugins.ui" },
