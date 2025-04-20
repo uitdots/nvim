63f28df762d7bb7b0dcb0d042d5b0abf1a16ca68
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-20 13:23:06 +0700

    chore(lsp): enable `postgres_ls` with new api temporary

--- lua/plugins/lsp/core/nvim-lspconfig.lua
+++ lua/plugins/lsp/core/nvim-lspconfig.lua
@@ -19,6 +19,8 @@ return {
     local servers = {
       -- NOTE: Add line by line the lsp you want to enable manually here (Install yourself, or "mason lspconfig" doesn't provide it yet, but neovim must support that)
       -- Name taken from https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+
+      "postgres_ls",
     }
 
     vim.lsp.enable(servers)
