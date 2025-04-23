0bcabb37eaa18a0b4ba723af6728abd1cc117364
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-23 18:59:54 +0700

    feat(plugins): add nvim-spider but disable

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -8,7 +8,9 @@ return {
         enable = true,
       },
       diagnostics = {
-        globals = { "vim" },
+        globals = {
+          "vim",
+        },
       },
       telemetry = {
         enable = false,
