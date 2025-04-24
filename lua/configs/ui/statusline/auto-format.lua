4ce30f594228110bf773a7c0cb868866567a3c11
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-24 21:26:01 +0700

    chore(stl): auto-format icon has more space

--- lua/configs/ui/statusline/auto-format.lua
+++ lua/configs/ui/statusline/auto-format.lua
@@ -1,9 +1,9 @@
 local function render()
   if vim.b.disable_autoformat then
-    return "%#St_gitIcons#󰁨 "
+    return "%#St_gitIcons#󰁨  "
   end
   if not vim.b.disable_autoformat and not vim.g.disable_autoformat then
-    return "%#St_Lsp#󰁨 "
+    return "%#St_Lsp#󰁨  "
   end
   return ""
 end
