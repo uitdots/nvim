b0c6741046bb0aaf549ada751132a7188fef8eb0
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-24 10:39:32 +0700

    fix(snacks): find files exclude ignore defined pattern, no ignore git

--- lua/plugins/neccessaries/snacks.lua
+++ lua/plugins/neccessaries/snacks.lua
@@ -98,7 +98,30 @@ return {
     {
       "<leader><leader>",
       function()
-        Snacks.picker.files({ hidden = true })
+        Snacks.picker.files({
+          hidden = true,
+          ignored = true,
+          exclude = {
+            "node_modules",
+            ".git",
+            ".egg-info",
+            "*bin*",
+            "*cache*",
+            "*debug*",
+            "*obj*",
+            ".dart_tool",
+            ".git",
+            ".husky/_",
+            ".idea",
+            ".next",
+            ".venv*",
+            ".vs",
+            "__pycache__",
+            "build",
+            "dist",
+            "node_modules",
+          },
+        })
       end,
       desc = "Find | Files",
     },
