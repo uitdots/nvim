e425c5b8023af39cfebe75c27557b91a6d12cd08
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-24 18:26:13 +0700

    fix(nvim-lint): haven't load linter by ft

--- lua/configs/editor/nvim-lint.lua
+++ lua/configs/editor/nvim-lint.lua
@@ -21,6 +21,19 @@ function M._config_linters()
   )
 end
 
+---If this is used, it will not have ability to live reload (we called it, not actually right...)
+---@private
+function M._filter_linters_by_ft()
+  for filetype, linters in pairs(M._linters_by_ft) do
+    M._linters_by_ft[filetype] = vim.tbl_filter(function(linter)
+      if lint.linters[linter] == nil then
+        return false
+      end
+      return is_executable(lint.linters[linter].cmd)
+    end, linters)
+  end
+end
+
 ---@private
 ---@type table<string, string[]>
 M._linters_by_ft = {
@@ -71,16 +84,8 @@ M._linters_by_ft = {
   },
 }
 
----@private
-function M._filter_linters_by_ft()
-  for filetype, linters in pairs(M._linters_by_ft) do
-    lint.linters_by_ft[filetype] = vim.tbl_filter(function(linter)
-      if lint.linters[linter] == nil then
-        return false
-      end
-      return is_executable(lint.linters[linter].cmd)
-    end, linters)
-  end
+function M._config_linters_by_ft()
+  lint.linters_by_ft = M._linters_by_ft
 end
 
 ---@private
@@ -89,7 +94,7 @@ function M._setup_autocommands()
     callback = debounce(function()
       require("lint").try_lint(nil, { ignore_errors = true })
       -- require("lint").try_lint "codespell"
-    end, 200),
+    end, 1000),
   })
 end
 
@@ -97,6 +102,7 @@ function M.setup()
   M._config_linters()
   -- FIXME: Currently use ignore_errors = true to ignore those linter doesn't exist. But we want to filter those first for runtime perf
   -- M._filter_linters_by_ft()
+  M._config_linters_by_ft()
   M._setup_autocommands()
 end
 
