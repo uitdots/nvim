51cb8a3b0a30b0af89cca8b318481cddad253e9b
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-25 22:50:04 +0700

    chore(dash): remove desc in dash

--- lua/configs/ui/nvdash/headers/uitvim.lua
+++ lua/configs/ui/nvdash/headers/uitvim.lua
@@ -8,7 +8,7 @@ local header = {
   "╚██████╔╝██║   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║",
   " ╚═════╝ ╚═╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝",
   "",
-  os() .. "Rescue your Neovim configurations ",
+  os(),
   "",
 }
 
