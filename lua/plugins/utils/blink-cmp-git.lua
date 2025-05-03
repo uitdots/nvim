b4a5a7e6435958b2f4b87411598c398f6dfec854
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-03 19:37:20 +0700

    chore(blink-cmp-git): really bad...

--- lua/plugins/utils/blink-cmp-git.lua
+++ lua/plugins/utils/blink-cmp-git.lua
@@ -2,36 +2,37 @@
 
 ---@type NvPluginSpec
 return {
-  "saghen/blink.cmp",
-  optional = true,
-  ---@type blink.cmp.Config
-  opts = {
-    sources = {
-      per_filetype = {
-        gitcommit = {
-          "git",
-          inherit_defaults = true,
+  "Kaiser-Yang/blink-cmp-git",
+  enabled = true,
+  ft = "gitcommit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "saghen/blink.cmp",
+      optional = true,
+      ---@type blink.cmp.Config
+      opts = {
+        sources = {
+          per_filetype = {
+            gitcommit = {
+              "git",
+              inherit_defaults = true,
+            },
+          },
+          providers = {
+            git = {
+              module = "blink-cmp-git",
+              name = "Git",
+              --- @module 'blink-cmp-git'
+              --- @type blink-cmp-git.Options
+              opts = {},
+            },
+          },
         },
       },
-      providers = {
-        git = {
-          module = "blink-cmp-git",
-          name = "Git",
-          --- @module 'blink-cmp-git'
-          --- @type blink-cmp-git.Options
-          opts = {},
-        },
+      opts_extend = {
+        "sources.per_filetype.gitcommit",
       },
-    },
-  },
-  opts_extend = {
-    "sources.per_filetype.gitcommit",
-  },
-  {
-    "Kaiser-Yang/blink-cmp-git",
-    ft = "gitcommit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
     },
   },
 }
