a9adb2921fe36162766ec0e52caa151b4df945ba
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-07 11:39:48 +0700

    feat(plugins/java): add `JavaHello/spring-boot.nvim`

--- lua/plugins/lsp/wrappers/jdtls.lua
+++ lua/plugins/lsp/wrappers/jdtls.lua
@@ -2,7 +2,24 @@ local is_executable = require("utils.executable").is_executable
 
 ---@type NvPluginSpec
 return {
-  "mfussenegger/nvim-jdtls",
-  cond = is_executable("jdtls"),
-  ft = "java",
+  {
+    "JavaHello/spring-boot.nvim",
+    cond = is_executable("jdtls"),
+    ft = "java",
+    opts = function()
+      local opts = {
+        init_options = {
+          bundles = vim.list_extend({}, require("spring_boot").java_extensions()),
+        },
+      }
+      return opts
+    end,
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      {
+        "ibhagwan/fzf-lua",
+        optional = true,
+      },
+    },
+  },
 }
