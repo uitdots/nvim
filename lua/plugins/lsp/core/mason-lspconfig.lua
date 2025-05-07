8cfa61eeff9a082701c9df13d7a6e9469ee2ee37
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-07 22:41:12 +0700

    feat(mason-lspconfig): update opts adapt new opts config

--- lua/plugins/lsp/core/mason-lspconfig.lua
+++ lua/plugins/lsp/core/mason-lspconfig.lua
@@ -1,3 +1,8 @@
+-- TODO: Do I need to check if mason has that's lsp (which neovim support but mason lspconfig hasn't?)
+
+local exclude_servers = vim.list_extend({}, require("uitvim").old_lsps)
+vim.list_extend(exclude_servers, require("uitvim").no_setup_lsps)
+
 ---@type NvPluginSpec
 return {
   "mason-org/mason-lspconfig.nvim",
@@ -5,29 +10,11 @@ return {
     "BufReadPost",
     "BufNewFile",
   },
-  -- NOTE: Change this to normal table when lsp is fully supported
-  opts = function(_, opts)
-    local old_server_names = require("uitvim").old_lsps
-    local no_setup_server_names = require("uitvim").no_setup_lsps
-    local mason_available_server_names = require("mason-lspconfig.mappings").get_mason_map().lspconfig_to_package
-
-    opts = opts or {}
-    opts.handlers = {
-      function(server_name)
-        vim.lsp.enable(server_name)
-      end,
-    }
-
-    -- Ye I know this is duplicate code but I hope there's a better way to iterate multiple table once
-    for _, server_name in pairs(old_server_names) do
-      if mason_available_server_names[server_name] ~= nil then
-        opts.handlers[server_name] = function() end
-      end
-    end
-    for _, server_name in pairs(no_setup_server_names) do
-      if mason_available_server_names[server_name] ~= nil then
-        opts.handlers[server_name] = function() end
-      end
-    end
-  end,
+  ---@module 'mason-lspconfig'
+  ---@type MasonLspconfigSettings
+  opts = {
+    automatic_enable = {
+      exclude = exclude_servers,
+    },
+  },
 }
