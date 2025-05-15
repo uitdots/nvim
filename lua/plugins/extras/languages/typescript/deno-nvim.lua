283e52ca0db4f59dcac895d86b38f045de087683
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-15 20:22:46 +0700

    chore(typescript): add `deno-nvim`

---
+++ lua/plugins/extras/languages/typescript/deno-nvim.lua
@@ -0,0 +1,16 @@
+local lspconfig = require("configs.lsp.lspconfig")
+
+---@type NvPluginSpec
+return {
+  "sigmaSd/deno-nvim",
+  enabled = false, -- TODO: Check this when it support neovim 0.11 API
+  opts = {
+    ---@type vim.lsp.Config
+    ---@diagnostic disable-next-line: missing-fields
+    server = {
+      on_attach = lspconfig.on_attach,
+      capabilites = lspconfig.capabilities,
+      on_init = lspconfig.on_init,
+    },
+  },
+}
