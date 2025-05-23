0e47200c3692e03794ee2236ba2f1d802ec08445
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-23 08:15:07 +0700

    chore: move lazyvim to config, not in init

--- init.lua
+++ init.lua
@@ -1,27 +1,8 @@
+require("uitvim")
 require("core.globals")
 require("core.env")
 require("core.options")
-
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_config = require("configs.core.lazy")
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-  },
-  { import = "plugins" },
-}, lazy_config)
-
+require("configs.lazy")
 require("nvchad.autocmds")
 require("core.highlight")
 require("core.filetypes")
