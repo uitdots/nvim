03bb25fb4c209a6d071f8d8d108620239fa4d101
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-24 11:09:01 +0700

    chore(vim-matchup): disable highlight

--- lua/plugins/main/editor/vim-matchup.lua
+++ lua/plugins/main/editor/vim-matchup.lua
@@ -2,6 +2,16 @@
 return {
   "andymass/vim-matchup",
   enabled = true,
+  event = {
+    "BufReadPost",
+    "BufNewFile",
+  },
+  init = function()
+    vim.g.matchup_matchparen_offscreen = {
+      method = "", ---@type "status" | "popup" | "status_manual" | "scrolloff" | ""
+    }
+    vim.g.matchup_matchparen_enabled = 0 ---@type 0 | 1 Disable matchup highlight because I don't like it
+  end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -11,13 +21,4 @@ return {
       },
     },
   },
-  event = {
-    "BufReadPost",
-    "BufNewFile",
-  },
-  init = function()
-    vim.g.matchup_matchparen_offscreen = {
-      method = "", ---@type "status" | "popup" | "status_manual" | "scrolloff" | ""
-    }
-  end,
 }
