6c7f099d645024148dcd254caa77792b19aa53cb
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-24 11:05:53 +0700

    feat(git): add co-author plugin

---
+++ lua/plugins/extras/git/co-author.lua
@@ -0,0 +1,15 @@
+---@type NvPluginSpec
+return {
+  "2kabhishek/co-author.nvim",
+  cmd = {
+    "CoAuthor",
+  },
+  keys = {
+    {
+      "<leader>gC",
+      "<cmd>CoAuthor<CR>",
+      desc = "CoAuthor | Find",
+      silent = true,
+    },
+  },
+}
