cb625cb42e624bae5955664f47f29014bf50a843
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-03 09:01:03 +0700

    chore(dropbar): path trunc

--- lua/plugins/extras/lsp/dropbar.lua
+++ lua/plugins/extras/lsp/dropbar.lua
@@ -6,6 +6,19 @@ return {
     "BufReadPost",
     "BufNewFile",
   },
+  ---@module 'dropbar'
+  ---@type dropbar_configs_t
+  opts = {
+    -- TODO: Maybe filter for java kotlin
+    sources = {
+      path = {
+        -- max_depth = 16,
+      },
+    },
+    bar = {
+      truncate = true,
+    },
+  },
   -- optional, but required for fuzzy finder support
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
