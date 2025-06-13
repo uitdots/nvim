85c8c218c23db938db57c1374897210bbdf36c5b
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-14 00:01:00 +0700

    chore(copilot): config enable suggestion auto trigger

--- lua/plugins/extras/ai/copilot.lua
+++ lua/plugins/extras/ai/copilot.lua
@@ -1,3 +1,5 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 ---@type NvPluginSpec
 return {
   "zbirenbaum/copilot.lua",
@@ -8,18 +10,32 @@ return {
       enabled = false,
     },
     suggestion = {
-      enabled = false,
+      enabled = true,
+      auto_trigger = true,
+    },
+    server_opts_overrides = {
+      settings = {
+        telemetry = {
+          telemetryLevel = "off",
+        },
+      },
     },
   },
   keys = {
     {
       "<leader>at",
       "<cmd>Copilot toggle<cr>",
-      desc = "AI | Toggle Copilot Completion",
+      desc = "AI | Toggle Copilot",
       silent = true,
     },
     {
-      "<leader>aP",
+      "<leader>as",
+      "<cmd>Copilot suggestion<cr>",
+      desc = "AI | Toggle Copilot Sugesstion",
+      silent = true,
+    },
+    {
+      "<leader>ap",
       "<cmd>Copilot panel<cr>",
       desc = "AI | Toggle Copilot Panel",
       silent = true,
@@ -32,35 +48,46 @@ return {
   dependencies = {
     "saghen/blink.cmp",
     optional = true,
-    ---@module 'blink-cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      sources = {
-        default = {
-          "copilot",
-        },
-        -- NOTE: If you custom blink source for more per_filetype, you have to declare here, and below "opts_extend"
-        per_filetype = {
-          lua = { "copilot" },
-          sql = { "copilot" },
-        },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      },
-    },
-    opts_extend = {
-      "sources.default",
-      "sources.per_filetype.lua",
-      "sources.per_filetype.sql",
-    },
-    dependencies = {
-      "fang2hou/blink-copilot",
-    },
+    ------TODO: Maybe should... check the disable diagnostic here
+    ------
+    ------@module 'blink.cmp'
+    ------@param opts blink.cmp.Config?
+    ------@return blink.cmp.Config
+    ---opts = function(_, opts)
+    ---  opts = opts or {}
+    ---  opts.keymap = opts.keymap or {}
+    ---
+    ---  local copilot = {
+    ---    function()
+    ---      if not require("copilot.suggestion").is_visible() then
+    ---        return require("copilot.suggestion").accept()
+    ---      end
+    ---    end,
+    ---  }
+    ---
+    ---  if opts.keymap["<Tab>"] == nil then
+    ---    opts.keymap["<Tab>"] = copilot
+    ---  else
+    ---    vim.list_extend(opts.keymap["<Tab>"], copilot, 1)
+    ---  end
+    ---
+    ---  vim.print(opts)
+    ---  return opts
+    ---end,
+    init = function()
+      autocmd("User", {
+        pattern = "BlinkCmpMenuOpen",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
+      })
+
+      autocmd("User", {
+        pattern = "BlinkCmpMenuClose",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
+    end,
   },
 }
