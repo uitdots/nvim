5fb3f8c089ba2aadc5ee6c0f536d8087e8c54584
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-16 02:29:47 +0700

    feat: configuring mcp
    relate: #2

--- lua/plugins/extras/ai/mcphub.lua
+++ lua/plugins/extras/ai/mcphub.lua
@@ -1,12 +1,47 @@
 local is_executable = require("utils.executable").is_executable_cache
+local border_enabled = require("uitvim").options.border_enabled
 
 ---@type NvPluginSpec
 return {
-  "ravitemer/mcphub.nvim",
-  cond = is_executable("npm"),
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  {
+    "ravitemer/mcphub.nvim",
+    cond = is_executable("npm"),
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      {
+        "<leader>am",
+        "<cmd>MCPHub<CR>",
+        desc = "AI | MCPHub",
+        silent = true,
+      },
+    },
+    opts = {
+      auto_approve = false,
+      auto_toggle_mcp_servers = true,
+      ui = {
+        window = {
+          border = border_enabled and "rounded" or "none", ---@type "none" | "single" | "double" | "rounded" | "solid" | "shadow"
+        },
+      },
+    },
+    build = "npm install -g mcp-hub@latest",
+    main = "mcphub",
   },
-  build = "npm install -g mcp-hub@latest",
-  main = "mcphub",
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true,
+            make_vars = true,
+            make_slash_commands = true,
+          },
+        },
+      },
+    },
+  },
 }
