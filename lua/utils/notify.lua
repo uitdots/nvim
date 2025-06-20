e46fe53e230b3b09d9929d7b2312df00ed9db27e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-20 14:03:34 +0700

    chore(utils): add another style icon and fix log level

--- lua/utils/notify.lua
+++ lua/utils/notify.lua
@@ -1,15 +1,17 @@
 local M = {}
 
 ---@private
+-- M.spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
 M.spinners = { "", "󰪞", "󰪟", "󰪠", "󰪡", "󰪢", "󰪣", "󰪤", "󰪥" }
 
 ---@private
+-- M.done = " "
 M.done = ""
 
 ---Process notification
 ---@param opts table { id: string, message: string, title: string, is_done: boolean }
 function M.processing(opts)
-  vim.notify(opts.message, "info", {
+  vim.notify(opts.message, vim.log.levels.INFO, {
     id = opts.id,
     title = opts.title,
     opts = function(notif)
