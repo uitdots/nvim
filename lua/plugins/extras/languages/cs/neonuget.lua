0bdcdc320447bf32068cf44091523ca0cba74a88
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-20 14:25:44 +0700

    feat(cs): add neonuget
    close: #19

---
+++ lua/plugins/extras/languages/cs/neonuget.lua
@@ -0,0 +1,16 @@
+---@type NvPluginSpec
+return {
+  "MonsieurTib/neonuget",
+  cmd = "NuGet",
+  keys = {
+    {
+      "<localleader>ln",
+      "<cmd>NuGet<CR>",
+      desc = "LPS | NuGet",
+    },
+  },
+  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
