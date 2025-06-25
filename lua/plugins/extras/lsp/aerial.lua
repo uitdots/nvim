f9ceff733468210521f32e4aa8e67c3d320740f8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-25 18:27:57 +0700

    refactor: use aerial to navigate instead having nvim-navbuddy

--- lua/plugins/extras/lsp/aerial.lua
+++ lua/plugins/extras/lsp/aerial.lua
@@ -1,6 +1,10 @@
 ---@type NvPluginSpec
 return {
   "stevearc/aerial.nvim",
+  cmd = {
+    "AerialToggle",
+    "AerialNavToggle",
+  },
   keys = {
     {
       "<leader>lo",
@@ -8,10 +12,51 @@ return {
       desc = "LSP | Outline",
       silent = true,
     },
+    {
+      "<leader>ln",
+      "<cmd>AerialNavToggle<cr>",
+      desc = "LSP | Nav",
+      silent = true,
+    },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
-  config = true,
+  opts = {
+    filter_kind = {
+      -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#symbolKind
+      "Array",
+      "Boolean",
+      "Class",
+      "Constant",
+      "Constructor",
+      "Enum",
+      "EnumMember",
+      "Event",
+      "Field",
+      "File",
+      "Function",
+      "Interface",
+      -- "Key",
+      "Method",
+      "Module",
+      "Namespace",
+      "Null",
+      "Number",
+      "Object",
+      -- "Operator",
+      "Package",
+      "Property",
+      "String",
+      "Struct",
+      -- "TypeParameter",
+      "Variable",
+    },
+    nav = {
+      win_opts = {
+        winblend = vim.o.winblend,
+      },
+    },
+  },
 }
