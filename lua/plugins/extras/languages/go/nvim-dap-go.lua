96d009d0afda67661f3a2c2553993dff1ba47c80
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-26 19:40:06 +0700

    fix(nvim-dap-go): make it as deps of nvim-dap

--- lua/plugins/extras/languages/go/nvim-dap-go.lua
+++ lua/plugins/extras/languages/go/nvim-dap-go.lua
@@ -1,22 +1,9 @@
 ---@type NvPluginSpec
 return {
-  {
-    ---This plugin is started with nvim-dap
+  "mfussenegger/nvim-dap",
+  optional = true,
+  dependencies = {
     "leoluz/nvim-dap-go",
-    config = function() end, -- Let nvim-dap call it to setup
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
-      local dap_go_spec = require("lazy.core.config").spec.plugins["nvim-dap-go"]
-      if not dap_go_spec then
-        return
-      end
-      local plugin = require("lazy.core.plugin")
-      local dap_go_opts = plugin.values(dap_go_spec, "opts", false)
-      require("dap-go").setup(dap_go_opts)
-    end,
-    dependencies = "leoluz/nvim-dap-go",
+    config = true,
   },
 }
