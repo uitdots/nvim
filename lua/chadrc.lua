254c9b92ee9396bfb3bf88537202ba2a1351d118
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-27 18:20:10 +0700

    chore(chadrc): show lazy status on dash

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -92,26 +92,26 @@ M.nvdash = {
       keys = "<Spc>nc",
       cmd = "NvCheatsheet",
     },
-    -- { txt = "─", no_gap = true, rep = true },
-    -- {
-    --   txt = function()
-    --     local ok, stats = pcall(function()
-    --       local lazy_stats = require("lazy").stats()
-    --       return {
-    --         loaded = lazy_stats.loaded,
-    --         count = lazy_stats.count,
-    --         milliseconds = math.floor(lazy_stats.startuptime),
-    --       }
-    --     end)
-    --     if ok then
-    --       return string.format("  Loaded %d/%d plugins in %d ms", stats.loaded, stats.count, stats.milliseconds)
-    --     else
-    --       return "󰇸 Cannot load lazy's status"
-    --     end
-    --   end,
-    --   hl = "NvDashFooter",
-    --   no_gap = true,
-    -- },
+    { txt = "─", no_gap = true, rep = true },
+    {
+      txt = function()
+        local ok, stats = pcall(function()
+          local lazy_stats = require("lazy").stats()
+          return {
+            loaded = lazy_stats.loaded,
+            count = lazy_stats.count,
+            milliseconds = math.floor(lazy_stats.startuptime),
+          }
+        end)
+        if ok then
+          return string.format("  Loaded %d/%d plugins in %d ms", stats.loaded, stats.count, stats.milliseconds)
+        else
+          return "󰇸 Cannot load lazy's status"
+        end
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
+    },
   },
 }
 
