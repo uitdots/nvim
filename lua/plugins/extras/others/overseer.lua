c0b8008d59f898b43e59c293525d115f0f0e7737
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-30 21:35:04 +0700

    chore(overseer): reconfig with lazy specs, add keymap and cmd last task

--- lua/plugins/extras/others/overseer.lua
+++ lua/plugins/extras/others/overseer.lua
@@ -1,103 +1,117 @@
 ---@type NvPluginSpec
 return {
-  {
-    "stevearc/overseer.nvim",
-    config = true,
-    cmd = {
-      "OverseerOpen",
-      "OverseerClose",
-      "OverseerToggle",
-      "OverseerSaveBundle",
-      "OverseerLoadBundle",
-      "OverseerDeleteBundle",
-      "OverseerRunCmd",
-      "OverseerRun",
-      "OverseerInfo",
-      "OverseerBuild",
-      "OverseerQuickAction",
-      "OverseerTaskAction",
-      "OverseerClearCache",
-    },
-    ---You must not declare to patch the dap here, see below to patch dap
-    ---@module 'overseer'
-    ---@type overseer.Config
-    opts = {
-      templates = { "builtin", "default" },
-      component_aliases = {
-        output = {
-          {
-            "open_output",
-            on_complete = "always",
-            direction = "dock",
-            focus = true,
-          },
+  "stevearc/overseer.nvim",
+  cmd = {
+    "OverseerOpen",
+    "OverseerClose",
+    "OverseerToggle",
+    "OverseerSaveBundle",
+    "OverseerLoadBundle",
+    "OverseerDeleteBundle",
+    "OverseerRunCmd",
+    "OverseerRun",
+    "OverseerInfo",
+    "OverseerBuild",
+    "OverseerQuickAction",
+    "OverseerTaskAction",
+    "OverseerClearCache",
+  },
+  ---You must not declare to patch the dap here, see below to patch dap
+  ---@module 'overseer'
+  ---@type overseer.Config
+  opts = {
+    templates = { "builtin", "default" },
+    component_aliases = {
+      output = {
+        {
+          "open_output",
+          on_complete = "always",
+          direction = "dock",
+          focus = true,
         },
       },
     },
-    keys = {
-      {
-        "<leader>rr",
-        "<cmd>OverseerRun<cr>",
-        desc = "Overseer | Run",
-        silent = true,
-      },
-      {
-        "<leader>rt",
-        "<cmd>OverseerToggle<cr>",
-        desc = "Overseer | Toggle",
-        silent = true,
-      },
-      {
-        "<leader>ri",
-        "<cmd>OverseerInfo<cr>",
-        desc = "Overseer | Info",
-        silent = true,
-      },
-      {
-        "<leader>rb",
-        "<cmd>OverseerBuild<cr>",
-        desc = "Overseer | Build",
-        silent = true,
-      },
-      {
-        "<leader>rA",
-        "<cmd>OverseerTaskAction<cr>",
-        desc = "Overseer | Run Task Action",
-        silent = true,
-      },
-      {
-        "<leader>ra",
-        "<cmd>OverseerQuickAction<cr>",
-        desc = "Overseer | Run quick action",
-        silent = true,
-      },
-      {
-        "<leader>rs",
-        "<cmd>OverseerSaveBundle<cr>",
-        desc = "Overseer | Save Bundle",
-        silent = true,
-      },
-      {
-        "<leader>rl",
-        "<cmd>OverseerLoadBundle<cr>",
-        desc = "Overseer | Load Bundle",
-        silent = true,
-      },
-      {
-        "<leader>rd",
-        "<cmd>OverseerDeleteBundle<cr>",
-        desc = "Overseer | Delete Bundle",
-        silent = true,
-      },
+  },
+  keys = {
+    {
+      "<leader>rr",
+      "<cmd>OverseerRun<cr>",
+      desc = "Overseer | Run",
+      silent = true,
+    },
+    {
+      "<leader>rt",
+      "<cmd>OverseerToggle<cr>",
+      desc = "Overseer | Toggle",
+      silent = true,
+    },
+    {
+      "<leader>ri",
+      "<cmd>OverseerInfo<cr>",
+      desc = "Overseer | Info",
+      silent = true,
+    },
+    {
+      "<leader>rb",
+      "<cmd>OverseerBuild<cr>",
+      desc = "Overseer | Build",
+      silent = true,
+    },
+    {
+      "<leader>rA",
+      "<cmd>OverseerTaskAction<cr>",
+      desc = "Overseer | Run Task Action",
+      silent = true,
+    },
+    {
+      "<leader>ra",
+      "<cmd>OverseerQuickAction<cr>",
+      desc = "Overseer | Run quick action",
+      silent = true,
+    },
+    {
+      "<leader>rs",
+      "<cmd>OverseerSaveBundle<cr>",
+      desc = "Overseer | Save Bundle",
+      silent = true,
+    },
+    {
+      "<leader>rL",
+      "<cmd>OverseerLoadBundle<cr>",
+      desc = "Overseer | Load Bundle",
+      silent = true,
+    },
+    {
+      "<leader>rL",
+      "<cmd>OverseerRestartLast<cr>",
+      desc = "Overseer | Restart Last",
+      silent = true,
+    },
+    {
+      "<leader>rd",
+      "<cmd>OverseerDeleteBundle<cr>",
+      desc = "Overseer | Delete Bundle",
+      silent = true,
     },
   },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
-      pcall(function()
+  init = function()
+    vim.api.nvim_create_user_command("OverseerRestartLast", function()
+      local overseer = require("overseer")
+      local tasks = overseer.list_tasks({ recent_first = true })
+      if vim.tbl_isempty(tasks) then
+        vim.notify("No tasks found", vim.log.levels.WARN)
+      else
+        overseer.run_action(tasks[1], "restart")
+      end
+    end, { desc = "Run last Overseer Task" })
+  end,
+  specs = {
+    {
+      "mfussenegger/nvim-dap",
+      optional = true,
+      opts = function()
         require("overseer").enable_dap(true)
-      end)
-    end,
+      end,
+    },
   },
 }
