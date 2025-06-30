d3128c1d5b38a6215516e1348d1f3a2cc50cdeae
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-30 22:25:43 +0700

    fix(vim-visual-multi): conflict CR with blink
    Relate: https://github.com/Saghen/blink.cmp/issues/406

--- lua/plugins/main/editor/vim-visual-multi.lua
+++ lua/plugins/main/editor/vim-visual-multi.lua
@@ -8,6 +8,7 @@ return {
   init = function()
     vim.g.VM_mouse_mappings = 1
     vim.g.VM_maps = {
+      ["I Return"] = "<S-CR>",
       ["Add Cursor Up"] = "<C-PageUp>", -- <C-Up> is for resize windows
       ["Add Cursor Down"] = "<C-PageDown>", -- <C-Down> is for resize windows
     }
