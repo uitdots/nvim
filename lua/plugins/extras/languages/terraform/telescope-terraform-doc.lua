e50fd7f6e319bcefed5eb4ebe95ca550aff48914
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-02 22:19:34 +0700

    feat(terraform): add terraform telescope and terraform telescope docs

---
+++ lua/plugins/extras/languages/terraform/telescope-terraform-doc.lua
@@ -0,0 +1,18 @@
+---@type NvPluginSpec
+return {
+  "ANGkeith/telescope-terraform-doc.nvim",
+  specs = {
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        extensions_list = {
+          "terraform_doc",
+        },
+      },
+      opts_extend = {
+        "extensions_list",
+      },
+      dependencies = "ANGkeith/telescope-terraform-doc.nvim",
+    },
+  },
+}
