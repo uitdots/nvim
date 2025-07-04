2f338b02c9984c35f53efcfd1493e28a5455e483
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-04 21:18:50 +0700

    refactor(nvim-lspconfig): move lspconfig into lazyspecs

--- lua/plugins/extras/lsp/nvim-lspconfig.lua
+++ lua/plugins/extras/lsp/nvim-lspconfig.lua
@@ -1,3 +1,25 @@
+local is_executable = require("utils.executable").is_executable
+local filter_availabled_external = require("uitvim").options.filter_availabled_external
+local lspconfig = require("configs.lsp.lspconfig")
+local lsps = require("uitvim").lsps
+
+---@param server_name string
+local function filter_executable(server_name)
+  local success, result = pcall(function()
+    is_executable(require("lspconfig.configs." .. server_name).default_config.cmd[1])
+  end)
+  return not success or result
+end
+
+local function setup_custom_lsps()
+  for _, server_name in pairs(lsps) do
+    if filter_availabled_external == false or filter_executable(server_name) then
+      vim.lsp.config(server_name, lspconfig.opts)
+      vim.lsp.enable(server_name)
+    end
+  end
+end
+
 ---@type NvPluginSpec
 return {
   "neovim/nvim-lspconfig",
@@ -14,6 +36,7 @@ return {
     },
   },
   config = function()
-    require("configs.lsp.nvim-lspconfig").setup()
+    lspconfig.setup()
+    setup_custom_lsps()
   end,
 }
