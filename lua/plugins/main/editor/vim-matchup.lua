4ef36ccaf79130b24161ec2cf4e29bea0c460be1
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-05 13:21:00 +0700

    chore(vim-matchup): eh...

--- lua/plugins/main/editor/vim-matchup.lua
+++ lua/plugins/main/editor/vim-matchup.lua
@@ -1,7 +1,6 @@
 ---@type NvPluginSpec
 return {
   "andymass/vim-matchup",
-  enabled = true,
   event = {
     "BufRead",
     "BufNewFile",
@@ -10,14 +9,17 @@ return {
     vim.g.matchup_matchparen_offscreen = {
       method = "", ---@type "status" | "popup" | "status_manual" | "scrolloff" | ""
     }
-    vim.g.matchup_matchparen_enabled = 0 ---@type 0 | 1 Disable matchup highlight because I don't like it
+    vim.g.matchup_matchparen_enabled = 1 ---@type 0 | 1 Toggle highlight
   end,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      matchup = {
-        enable = true,
-        enable_quotes = true,
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  specs = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        matchup = {
+          enable = true,
+          enable_quotes = true,
+        },
       },
     },
   },
