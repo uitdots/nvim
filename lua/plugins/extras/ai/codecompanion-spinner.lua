4775ed9e93ad8d826f460e5ebb4687d740b4b316
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-08 22:04:11 +0700

    chore(codecompanion-spinner): missing deps

--- lua/plugins/extras/ai/codecompanion-spinner.lua
+++ lua/plugins/extras/ai/codecompanion-spinner.lua
@@ -3,5 +3,8 @@ return {
   "franco-ruggeri/codecompanion-spinner.nvim",
   ft = "codecompanion",
   config = true,
-  dependencies = "olimorris/codecompanion.nvim",
+  dependencies = {
+    "olimorris/codecompanion.nvim",
+    "nvim-lua/plenary.nvim",
+  },
 }
