49d3c5028a4558d159fa6c5710a5ea82a1b1a4d8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-14 12:42:02 +0700

    chore(neoscroll): enable but faster

--- lua/plugins/extras/motion/neoscroll.lua
+++ lua/plugins/extras/motion/neoscroll.lua
@@ -2,10 +2,11 @@
 --NOTE: Smooth scrolling neovim plugin written in lua
 return {
   "karb94/neoscroll.nvim",
-  enabled = false,
+  enabled = true,
   event = "VeryLazy",
   opts = {
     hide_cursor = false,
+    duration_multiplier = 0.5, ---@type integer
     easing = "linear", ---@type "linear" | "quadratic" | "cubic" | "quartic" | "quintic" | "circular" | "sine"
   },
 }
