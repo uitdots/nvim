5127f391e254d96bfeb85dcbd2890f4c62bab568
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-14 20:19:01 +0700

    chore(yamlls): schema for argocd

--- after/lsp/yamlls.lua
+++ after/lsp/yamlls.lua
@@ -21,24 +21,51 @@ return {
           {
             description = "Docker compose for yaml.docker-compose",
             fileMatch = {
-              "**/*compose.override.yaml",
-              "**/*compose.override.yml",
-              "**/*compose.yaml",
-              "**/*compose.yml",
+              "*compose.override.yaml",
+              "*compose.override.yml",
+              "*compose.yaml",
+              "*compose.yml",
             },
             name = "docker-compose.yml",
             url = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
           },
+          -- kubernetes
           {
             fileMatch = "",
-            name = "",
+            name = "kubernetes",
             url = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/deployment.json",
           },
-          {
-            fileMatch = "",
-            name = "",
-            url = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
-          },
+          -- argocd, and yaml_ls cannot parse them bruh
+          -- {
+          --   fileMatch = "",
+          --   name = "argo workflows",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
+          -- },
+          -- {
+          --   fileMatch = "app-of-apps/apps/*.yaml",
+          --   name = "argocd kustomize",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-schema-generator/refs/heads/main/schema/argo_cd_kustomize_schema.json",
+          -- },
+          -- {
+          --   fileMatch = "",
+          --   name = "argocd all k8s kustomize",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-schema-generator/refs/heads/main/schema/argo_all_k8s_kustomize_schema.json",
+          -- },
+          -- {
+          --   fileMatch = "",
+          --   name = "argocd events kustomize",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-schema-generator/refs/heads/main/schema/argo_events_kustomize_schema.json",
+          -- },
+          -- {
+          --   fileMatch = "",
+          --   name = "argocd rollouts kustomize",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-schema-generator/refs/heads/main/schema/argo_rollouts_kustomize_schema.json",
+          -- },
+          -- {
+          --   fileMatch = "",
+          --   name = "argocd workflow kustomize",
+          --   url = "https://raw.githubusercontent.com/argoproj/argo-schema-generator/refs/heads/main/schema/argo_workflows_kustomize_schema.json",
+          -- },
         },
       }),
     },
