87e549e896c66058caef80810a79ab91ffb7bbe8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-18 19:53:06 +0700

    feat: nvim-various-textobjs, remove mini-ai

--- lua/plugins/extras/motion/nvim-treesitter-textobjects.lua
+++ lua/plugins/extras/motion/nvim-treesitter-textobjects.lua
@@ -11,10 +11,12 @@ return {
             enable = true,
             lookahead = true,
             keymaps = {
+              ["aC"] = { query = "@comment.outer", desc = "TSTextObject | Arround Comment" },
               ["aa"] = { query = "@parameter.outer", desc = "TSTextObject | Outer Argument/Parameter" },
               ["ac"] = { query = "@class.outer", desc = "TSTextObject | Arround Class" },
               ["af"] = { query = "@function.outer", desc = "TSTextObject | Arround Function/Method" },
               ["as"] = { query = "@local.scope", query_group = "locals", desc = "TSTextObject | Language Scope" },
+              ["iC"] = { query = "@comment.outer", desc = "TSTextObject | Inner Comment" },
               ["ia"] = { query = "@parameter.inner", desc = "TSTextObject | Inner Argument/Parameter" },
               ["ic"] = { query = "@class.inner", desc = "TSTextObject | Inner Class" },
               ["if"] = { query = "@function.inner", desc = "TSTextObject | Inner Function/Method" },
@@ -41,6 +43,7 @@ return {
             enable = true,
             set_jumps = true,
             goto_next_start = {
+              ["]C"] = { query = "@comment.outer", desc = "TSTextObject | Next Comment Start" },
               ["]S"] = { query = "@scope", query_group = "locals", desc = "TSTextObject | Next Scope Start" },
               ["]c"] = { query = "@class.outer", desc = "TSTextObject | Next Class Start" },
               ["]f"] = { query = "@function.outer", desc = "TSTextObject | Next Function/Method Start" },
@@ -50,6 +53,7 @@ return {
               ["]z"] = { query = "@fold", query_group = "folds", desc = "TSTextObject | Next Fold Start" },
             },
             goto_previous_start = {
+              ["[C"] = { query = "@comment.outer", desc = "TSTextObject | Previous Comment Start" },
               ["[S"] = { query = "@scope", query_group = "locals", desc = "TSTextObject | Previous Scope Start" },
               ["[c"] = { query = "@class.outer", desc = "TSTextObject | Previous Class Start" },
               ["[f"] = { query = "@function.outer", desc = "TSTextObject | Previous Function/Method Start" },
---
+++ lua/plugins/extras/motion/nvim-various-textobjs.lua
@@ -0,0 +1,68 @@
+---@type LazySpec
+return {
+  "chrisgrieser/nvim-various-textobjs",
+  event = "VeryLazy",
+  opts = {
+    keymaps = {
+      useDefaults = true,
+      disabledDefaults = {
+        "i,",
+        "a,",
+      },
+    },
+  },
+  keys = {
+    {
+      "gx",
+      function()
+        require("various-textobjs").url()
+        local foundURL = vim.fn.mode() == "v"
+        if not foundURL then
+          return
+        end
+        local url = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), { type = "v" })[1]
+        vim.ui.open(url)
+        vim.cmd.normal({ "v", bang = true })
+      end,
+      desc = "Various Textobjs | Smart URL Opener",
+      silent = true,
+    },
+    {
+      "gx",
+      function()
+        require("various-textobjs").filepath("outer")
+        local foundPath = vim.fn.mode() == "v"
+        if not foundPath then
+          return
+        end
+        local path = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), { type = "v" })[1]
+        local exists = vim.uv.fs_stat(vim.fs.normalize(path)) ~= nil
+        if exists then
+          vim.ui.open(path)
+        else
+          vim.notify("Path does not exist.", vim.log.levels.WARN, { title = "Open File", id = "open_file" })
+        end
+      end,
+      desc = "Various Textobjs | Smart File Opener",
+      silent = true,
+    },
+    {
+      "ie",
+      function()
+        require("various-textobjs").entireBuffer()
+      end,
+      desc = "Various Textobjs | Entire Buffer",
+      mode = { "x", "o" },
+      silent = true,
+    },
+    {
+      "ae",
+      function()
+        require("various-textobjs").entireBuffer()
+      end,
+      desc = "Various Textobjs | Entire Buffer",
+      mode = { "x", "o" },
+      silent = true,
+    },
+  },
+}
