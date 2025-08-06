41bd4ed9c0ef4364e2c97b2bf2631a972202a7ce
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-06 21:40:17 +0700

    feat: use go nvim but only snippets and treesitter inject

--- lua/plugins/extras/languages/go/go.lua
+++ lua/plugins/extras/languages/go/go.lua
@@ -1,49 +1,17 @@
--- I disable this plugin because I think it's enough to use normally :v
-
--- local filter_availabled_external = require("preferences").options.others.
--- local is_executable = require("utils.executable").is_executable_cache
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
 ---@type LazySpec
 return {
   "ray-x/go.nvim",
-  enabled = false,
-  -- enabled = not filter_availabled_external or is_executable("go"),
-  event = {
-    "CmdlineEnter",
-  },
   ft = {
     "go",
     "gomod",
   },
   opts = {
-    lsp_inlay_hints = {
-      enable = false,
-    },
-    diagnostic = false,
-    null_ls = {
-      golangci_lint = false,
-      gotest = false,
-    },
-    trouble = true,
-    luasnip = true,
+    disable_defaults = true,
+    go = "go",
   },
-  init = function()
-    local autocmd_group = augroup("GoFmtAndImport", {})
-
-    autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-        require("go.format").gofmt()
-      end,
-      group = autocmd_group,
-    })
+  config = function(_, opts)
+    require("go.commands").add_cmds = function() end
+    require("go").setup(opts)
+    require("snips.go")
   end,
-  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  dependencies = { -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
 }
