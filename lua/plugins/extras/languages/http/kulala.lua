efb15e2d4fd34380e2bb4286969843f5666e3d2b
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-14 17:47:56 +0700

    feat: totally remove rest.nvim and replaced with kulala

---
+++ lua/plugins/extras/languages/http/kulala.lua
@@ -0,0 +1,17 @@
+---@type LazySpec
+return {
+  "mistweaverco/kulala.nvim",
+  keys = {
+    { "<leader>qs", desc = "Send request" },
+    { "<leader>qa", desc = "Send all requests" },
+    { "<leader>qb", desc = "Open scratchpad" },
+  },
+  ft = {
+    "http",
+    "rest",
+  },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>q",
+  },
+}
