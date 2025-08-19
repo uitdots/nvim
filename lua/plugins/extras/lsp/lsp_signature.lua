5f5967f027f41978b08d7e0c135d0cde75d8be71
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-19 07:32:46 +0700

    feat: signature wiwth lsp_signature

--- lua/plugins/extras/lsp/lsp_signature.lua
+++ lua/plugins/extras/lsp/lsp_signature.lua
@@ -1,5 +1,22 @@
 ---@type LazySpec
 return {
   "ray-x/lsp_signature.nvim",
-  event = "VeryLazy",
+  event = "InsertEnter",
+  opts = {
+    bind = true,
+    handler_opts = {
+      border = vim.o.winborder,
+    },
+    hint_enable = false,
+    always_trigger = true,
+    toggle_key = "<C-k>",
+    select_signature_key = "<M-n>",
+    move_cursor_key = "<C-S-k>",
+    move_signature_window_key = {
+      "<M-j>",
+      "<M-k>",
+      "<M-l>",
+      "<M-h>",
+    },
+  },
 }
