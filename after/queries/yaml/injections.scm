7e0efd918c912e52192c1fc6db033475d998e263
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-28 23:02:15 +0700

    feat(yaml): inject more

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -43,10 +43,30 @@
 (block_mapping_pair
   (comment) @injection.language
   value: (block_node
+    (block_mapping
+      (block_mapping_pair
+        value: (block_node
+          (block_scalar) @injection.content))))
+  (#offset! @injection.content 0 1 0 0)
+  (#gsub! @injection.language ".*language?%s*[%:=]%s*([%w%p]+).*" "%1")
+  (#set! "priority" 110))
+
+(block_mapping
+  (comment) @injection.language
+  (block_mapping_pair
+    value: (block_node
+      (block_scalar) @injection.content))
+  (#offset! @injection.content 0 1 0 0)
+  (#gsub! @injection.language ".*language?%s*[%:=]%s*([%w%p]+).*" "%1")
+  (#set! "priority" 110))
+
+(block_mapping_pair
+  (comment) @injection.language
+  value: (block_node
     (block_sequence
       (block_sequence_item
         (flow_node
           (plain_scalar
-          (string_scalar) @injection.content)))))
+            (string_scalar) @injection.content)))))
   (#gsub! @injection.language ".*language?%s*[%:=]%s*([%w%p]+).*" "%1")
   (#set! "priority" 110))
