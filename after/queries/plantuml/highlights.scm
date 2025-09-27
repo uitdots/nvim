53865e8972481e8d2af0b18b13b2a25ee96495a8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-09-27 22:11:02 +0700

    feat(plantuml): more highlights

--- after/queries/plantuml/highlights.scm
+++ after/queries/plantuml/highlights.scm
@@ -10,16 +10,55 @@
  "$"
 ] @keyword
 
-(uniqkey) @keyword
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+  "<"
+  ">"
+] @punctuation.bracket
+
+(uniqkey) @variable
 
 (string) @string
 
-((identifier) @keyword
-  (#any-of? @keyword
-    "json"
-    "object"
-    "actor"
-    "class"))
+((identifier) @function
+  . (#any-of? @function
+    "autonumber"
+    "skinparam"))
+
+(command
+ . (identifier) @keyword
+ (_)* @string
+ (#any-of? @keyword
+   "activate"
+   "actor"
+   "alt"
+   "as"
+   "boundary"
+   "break"
+   "class"
+   "collections"
+   "control"
+   "critical"
+   "database"
+   "deactivate"
+   "else"
+   "end"
+   "entity"
+   "group"
+   "json"
+   "loop"
+   "note"
+   "object"
+   "package"
+   "par"
+   "participant"
+   "queue"
+   "rectangle"))
 
 (uniquevar
   (identifier) @type.definition)
@@ -33,18 +72,73 @@
   head: (identifier) @type.definition
   end: (identifier) @type.definition)
 
-[
-  "("
-  ")"
-  "{"
-  "}"
-  "["
-  "]"
-  "<"
-  ">"
-] @punctuation.bracket
-
 (block_1
   (identifier) @type.definition)
 
 (comment) @comment
+
+; a -- b
+; a --> b: bla
+; and more
+(command
+  . (identifier) @variable
+  . (uniqkey)* @punctuation.arrow
+  . (identifier) @variable
+  . (uniqkey)? @punctuation.delimiter
+  (_)* @string
+  (#eq? @punctuation.delimiter ":"))
+
+; a .. b
+; a ..> b: bla
+(command
+  . (identifier) @variable
+  . (uniqkey)* @punctuation.arrow
+  . (identifier) @punctuation.arrow
+  . (uniqkey)* @punctuation.arrow
+  . (identifier) @variable
+  . (uniqkey)? @punctuation.delimiter
+  (_)* @string
+  (#eq? @punctuation.delimiter ":"))
+
+; left to right direction
+(command
+  . (identifier) @constant
+  . (identifier) @keyword
+  . (identifier) @constant
+  . (identifier) @keyword.direction
+  (#eq? @keyword.direction "direction"))
+
+; note
+(command
+  . (identifier) @keyword.note
+  . (identifier) @constant.direction
+  . (identifier) @keyword.of
+  . (identifier) @variable
+  . (uniqkey) @punctuation.delimiter
+  (_)* @string
+  (#eq? @keyword.note "note")
+  (#any-of? @constant.direction
+    "down"
+    "left"
+    "right"
+    "top")
+  (#eq? @punctuation.delimiter ":"))
+
+; actor a as A
+(command
+  . (identifier) @keyword
+  . (identifier) @variable
+  . (identifier) @keyword.as
+  [
+    (identifier) @variable
+    (string)
+  ] (#eq? @keyword.as "as"))
+
+; activate a
+; deactivate a
+(command
+  . (identifier) @keyword
+  . (identifier) @variable
+  (#any-of? @keyword
+    "activate"
+    "deactivate"))
