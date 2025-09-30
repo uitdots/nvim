5dcfadd49b94af7c39adb44b445a1f642b25dc97
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-09-30 19:08:12 +0700

    chore(d2): conform no custom

--- lua/plugins/extras/languages/d2/conform.lua
+++ lua/plugins/extras/languages/d2/conform.lua
@@ -4,15 +4,6 @@ return {
   ---@module 'conform'
   ---@type conform.setupOpts
   opts = {
-    formatters = {
-      d2 = {
-        stdin = false,
-        args = {
-          "fmt",
-          "$FILENAME",
-        },
-      },
-    },
     formatters_by_ft = {
       d2 = {
         "d2",
