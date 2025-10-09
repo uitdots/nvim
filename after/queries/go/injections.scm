2c9178db3136fcf013bbcd6b9f74a2113d9d0e4e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-09 23:22:32 +0700

    feat(go): add tree-sitter-go_tags

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -3,4 +3,4 @@
 (field_declaration
   tag: (raw_string_literal
     (raw_string_literal_content) @injection.content
-    (#set! injection.self)))
+    (#set! injection.language "go_tags")))
---
+++ lua/plugins/extras/languages/go/tree-sitter-go_tags.lua
@@ -0,0 +1,28 @@
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  ---@module 'nvim-treesitter'
+  ---@param opts PluginsOpts.TSConfig?
+  opts = function(_, opts)
+    opts = opts or {}
+    opts.ensure_installed = opts.ensure_installed or {}
+    ---@diagnostic disable-next-line: param-type-mismatch
+    table.insert(opts.ensure_installed, "go_tags")
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = function()
+        ---@diagnostic disable-next-line: inject-field, missing-fields
+        require("nvim-treesitter.parsers").go_tags = {
+          ---@diagnostic disable-next-line: missing-fields
+          install_info = {
+            url = "https://github.com/DanWlker/tree-sitter-go_tags",
+            branch = "tree-sitter-1.25.5",
+            queries = "queries",
+          },
+        }
+      end,
+    })
+    return opts
+  end,
+}
