4faa3d0f893b34c89e12c59dd449d289199bae0a
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-13 18:24:43 +0700

    chore(vim-dadbod-ui): setup ui a bit

--- lua/plugins/extras/database/vim-dadbod-ui.lua
+++ lua/plugins/extras/database/vim-dadbod-ui.lua
@@ -11,6 +11,9 @@ return {
     vim.g.db_ui_execute_on_save = 0
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.vim_dadbod_completion_mark = ""
+    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_show_help = 0
+    vim.g.db_ui_show_database_icon = 1
   end,
   keys = {
     {
