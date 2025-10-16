1a2b3cd6959f83aad54d474edd4f9a2e406c265d
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-17 02:26:36 +0700

    feat: mise toml integration

--- lua/plugins/extras/languages/toml/nvim-treesitter.lua
+++ lua/plugins/extras/languages/toml/nvim-treesitter.lua
@@ -1,13 +1,17 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  ---@type PluginsOpts.TSConfig
-  opts = {
-    ensure_installed = {
-      "toml",
-    },
-  },
-  opts_extend = {
-    "ensure_installed",
-  },
+  ---@param opts? PluginsOpts.TSConfig
+  opts = function(_, opts)
+    opts = opts or {}
+    opts.ensure_installed = opts.ensure_installed or {}
+    ---@diagnostic disable-next-line: param-type-mismatch
+    table.insert(opts.ensure_installed, "toml")
+
+    require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+      local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+      local filename = vim.fn.fnamemodify(filepath, ":t")
+      return string.match(filename, ".*mise.*%.toml$") ~= nil
+    end, { force = true, all = false })
+  end,
 }
