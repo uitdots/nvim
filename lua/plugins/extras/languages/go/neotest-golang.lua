f7ccb60a452e744aa65985b0fbef96bd1e538308
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-17 03:00:13 +0700

    chore(neotest-golang): export the out test file

--- lua/plugins/extras/languages/go/neotest-golang.lua
+++ lua/plugins/extras/languages/go/neotest-golang.lua
@@ -10,7 +10,18 @@ return {
           opts = function(_, opts)
             opts = opts or {}
             opts.adapters = opts.adapters or {}
-            table.insert(opts.adapters, require("neotest-golang")())
+            ---@module 'neotest-golang'
+            ---@type NeotestGolangOptions
+            ---@diagnostic disable-next-line: missing-fields
+            local test_opts = {
+              go_test_args = {
+                "-v",
+                "-race",
+                "-count=1",
+                "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+              },
+            }
+            table.insert(opts.adapters, require("neotest-golang")(test_opts))
           end,
         },
       },
