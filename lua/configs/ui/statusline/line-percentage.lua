804b738460e735df73eed9f54a834f257a49410e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-21 21:26:35 +0700

    refactor: line-percentage faster

--- lua/configs/ui/statusline/line-percentage.lua
+++ lua/configs/ui/statusline/line-percentage.lua
@@ -5,7 +5,7 @@ local M = {}
 M.state = nil
 
 local function gen_block(icon, txt, sep_l_hlgroup, iconHl_group, txt_hl_group, sep_l, sep_end)
-  return sep_l_hlgroup .. sep_l .. iconHl_group .. icon .. " " .. txt_hl_group .. " " .. txt .. sep_end
+  return string.format("%s%s%s%s %s %s %s", sep_l_hlgroup, sep_l, iconHl_group, icon, txt_hl_group, txt, sep_end)
 end
 
 local function set_state()
@@ -44,24 +44,22 @@ local function set_state()
   local separators = (type(sep_style) == "table" and sep_style) or mode[theme][sep_style]
 
   local sep_l = separators["left"]
-  local sep_end = "%#St_sep_r#" .. separators["right"]
+  local sep_end = string.format("%%#St_sep_r#%s", separators["right"])
 
   if theme == "default" then
-    M.state = "%#St_Percent_sep#" .. sep_l .. "%#St_Percent_icon# %#St_Percent_text# %p %% "
-    return
+    M.state = string.format("%%#St_Percent_sep#%s%%#St_Percent_icon# %%#St_Percent_text# %%p %%%% ", sep_l)
   elseif theme == "vscode" or theme == "vscode_colored" then
     M.state = "%#StText# %L"
-    return
+  else
+    M.state = gen_block("", "%L", "%#St_Percent_sep#", "%#St_Percent_bg#", "%#St_Percent_txt#", sep_l, sep_end)
   end
-
-  M.state = gen_block("", "%L", "%#St_Percent_sep#", "%#St_Percent_bg#", "%#St_Percent_txt#", sep_l, sep_end)
 end
 
----@return string?
+---@return string
 return function()
-  if M.state == nil then
-    set_state()
+  if M.state ~= nil then
+    return M.state
   end
-
+  set_state()
   return M.state
 end
