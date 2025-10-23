6496ede166ea8ecf3514fcce3702efb16555d00a
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-23 23:41:29 +0700

    chore(kulala): add key for trigger manage auth config

--- lua/plugins/extras/languages/http/kulala.lua
+++ lua/plugins/extras/languages/http/kulala.lua
@@ -5,6 +5,7 @@ return {
     { "<leader>qs", desc = "Send request" },
     { "<leader>qa", desc = "Send all requests" },
     { "<leader>qb", desc = "Open scratchpad" },
+    { "<leader>qu", desc = "Manage Auth Config" },
   },
   ft = {
     "http",
@@ -13,5 +14,8 @@ return {
   opts = {
     global_keymaps = true,
     global_keymaps_prefix = "<leader>q",
+    lsp = {
+      formatter = true,
+    },
   },
 }
