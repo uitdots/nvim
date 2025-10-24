f14c132f2473ea4c57b9fc202e1295b5a174d384
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-24 22:57:21 +0700

    feat(yamlls): not trigger for gh action and compose file

--- after/lsp/yamlls.lua
+++ after/lsp/yamlls.lua
@@ -1,5 +1,16 @@
 ---@type vim.lsp.Config
 return {
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    if fname:match(".*compose.*%.ya?ml") then
+      return
+    end
+    local parent = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr))
+    if vim.endswith(parent, "/.github/workflows") or vim.endswith(parent, "/.forgejo/workflows") or vim.endswith(parent, "/.gitea/workflows") then
+      return
+    end
+    on_dir()
+  end,
   ---@module 'codesettings'
   ---@type lsp.yamlls
   settings = {
