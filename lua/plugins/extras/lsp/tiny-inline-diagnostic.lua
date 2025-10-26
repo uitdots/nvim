3b4cf588125cf3126b236f2d460043241b4f816e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-26 20:24:11 +0700

    chore(tiny-inline-diagnostic): no custom opts

--- lua/plugins/extras/lsp/tiny-inline-diagnostic.lua
+++ lua/plugins/extras/lsp/tiny-inline-diagnostic.lua
@@ -1,14 +1,6 @@
 ---@type LazySpec
--- NOTE: Show Better Diagnostic Inline
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VeryLazy",
-  enabled = true,
-  opts = {
-    options = {
-      overflow = {
-        mode = "wrap", ---@type "wrap" | "none" | "oneline"
-      },
-    },
-  },
+  config = true,
 }
