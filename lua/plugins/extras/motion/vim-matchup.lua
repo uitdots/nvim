9d20af58d5893eb9d5cb919f737e11777a22b776
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-10-27 20:50:21 +0700

    fix(vim-matchup): oh it has lua, we should lazy load it

--- lua/plugins/extras/motion/vim-matchup.lua
+++ lua/plugins/extras/motion/vim-matchup.lua
@@ -3,10 +3,19 @@ local g = vim.g
 ---@type LazySpec
 return {
   "andymass/vim-matchup",
+  event = {
+    "BufRead",
+    "BufNewFile",
+  },
   init = function()
+    g.loaded_matchit = 1
     g.matchup_matchparen_offscreen = {
       method = "", ---@type "status" | "popup" | "status_manual" | "scrolloff" | ""
     }
     g.matchup_matchparen_enabled = 0 ---@type 0 | 1 Toggle highlight
   end,
+  ---@module 'match-up'
+  ---@type matchup.Config
+  opts = nil,
+  config = true,
 }
