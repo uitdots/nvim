5a206639f142b8e363702324e21bf43620cf08a6
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-11-08 00:18:48 +0700

    feat(nvim-treesitter): add zsh

--- lua/plugins/extras/languages/shell/nvim-treesitter.lua
+++ lua/plugins/extras/languages/shell/nvim-treesitter.lua
@@ -1,18 +1,14 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  ---@module 'nvim-treesitter'
-  ---@param opts TSConfig?
-  ---@return TSConfig
-  opts = function(_, opts)
-    vim.treesitter.language.register("bash", "zsh")
-
-    opts = opts or {}
-    if opts.ensure_installed then
-      opts.ensure_installed[#opts.ensure_installed + 1] = "bash"
-    else
-      opts.ensure_installed = { "bash" }
-    end
-    return opts
-  end,
+  ---@type PluginsOpts.TSConfig
+  opts = {
+    ensure_installed = {
+      "bash",
+      "zsh",
+    },
+  },
+  opts_extend = {
+    "ensure_installed",
+  },
 }
