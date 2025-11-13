4aabf3fe8dd91f48a6240b8f555c53e3235d9cab
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-11-13 13:51:16 +0700

    fix(neotest-playwright): find executable for playwright bin

--- lua/plugins/languages/typescript/neotest-playwright.lua
+++ lua/plugins/languages/typescript/neotest-playwright.lua
@@ -9,6 +9,16 @@ return {
         "NeotestPlaywrightProject",
         "NeotestPlaywrightRefresh",
       },
+      keys = {
+        {
+          "<leader>T<leader>a",
+          function()
+            ---@diagnostic disable-next-line: undefined-field
+            require("neotest").playwright.attachment()
+          end,
+          desc = "Neotest | Playwright Show Attachment",
+        },
+      },
       specs = {
         {
           "nvim-neotest/neotest",
@@ -19,8 +29,15 @@ return {
 
             local adapter_opts = {
               options = {
-                persist_project_selection = true,
+                persist_project_selection = false,
                 enable_dynamic_test_discovery = true,
+                get_playwright_binary = function()
+                  local bin_path = vim.fn.exepath("playwright")
+                  if bin_path ~= "" then
+                    return bin_path
+                  end
+                  return vim.loop.cwd() .. "/node_modules/.bin/playwright"
+                end,
               },
             }
             table.insert(opts.adapters, require("neotest-playwright").adapter(adapter_opts))
