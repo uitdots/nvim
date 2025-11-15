baabea05633f200dab2a4bb8e8de5f3f87f6d293
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-11-15 23:29:10 +0700

    feat(ai): use vectorcode

--- lua/plugins/ai/vectorcode.lua
+++ lua/plugins/ai/vectorcode.lua
@@ -1,33 +1,38 @@
 ---@type LazySpec
 return {
   "Davidyz/VectorCode",
-  enabled = false,
   version = "*",
-  -- build = "uv tool upgrade 'vectorcode[lsp]'",
-  build = "uv tool upgrade vectorcode",
+  build = "uv tool upgrade 'vectorcode[lsp,mcp,intel]'",
   ---@module 'vectorcode'
   ---@type VectorCode.Opts
   ---@diagnostic disable-next-line: missing-fields
   opts = {
-    async_backend = "default",
+    async_backend = "lsp",
+    ---@diagnostic disable-next-line: missing-fields
+    on_setup = {
+      lsp = true,
+    },
   },
   dependencies = "nvim-lua/plenary.nvim",
   specs = {
     {
       "olimorris/codecompanion.nvim",
-      opts = {
-        extensions = {
-          vectorcode = {
-            opts = {
-              add_tool = true,
-              add_slash_command = true,
+      opts = function(_, opts)
+        return vim.tbl_deep_extend("force", opts or {}, {
+          extensions = {
+            vectorcode = {
+              ---@module 'vectorcode'
+              ---@type VectorCode.CodeCompanion.ExtensionOpts
+              opts = {
+                add_tool = true,
+                add_slash_command = true,
+              },
+              ---@type VectorCode.CodeCompanion.ToolOpts
+              tool_opts = nil,
             },
-            ---@module 'vectorcode'
-            ---@type VectorCode.CodeCompanion.ToolOpts
-            tool_opts = nil,
           },
-        },
-      },
+        })
+      end,
       dependencies = "Davidyz/VectorCode",
       optional = true,
     },
