995998b88f99963b2802fba02adccc51147eed22
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-12-28 19:03:08 +0700

    fix(kotlin_lsp): no trigger on gradle config file

--- after/lsp/kotlin_lsp.lua
+++ after/lsp/kotlin_lsp.lua
@@ -1,19 +1,13 @@
-local allowed_filenames = {
-  ["build.gradle"] = true,
+local disallowed_filenames = {
   ["build.gradle.kts"] = true,
-  ["settings.gradle"] = true,
   ["settings.gradle.kts"] = true,
 }
 
 ---@type vim.lsp.Config
 return {
-  filetypes = {
-    "groovy",
-    "kotlin",
-  },
   root_dir = function(_, on_dir)
     local fname = vim.fn.expand("%:t")
-    if allowed_filenames[fname] then
+    if not disallowed_filenames[fname] then
       on_dir()
     end
   end,
