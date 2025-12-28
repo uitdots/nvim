595b65116eb0e9623b7ec7f04c5fcff4a0274748
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-12-28 22:20:18 +0700

    feat(java/neotest-java): ok

--- lua/plugins/languages/java/neotest-java.lua
+++ lua/plugins/languages/java/neotest-java.lua
@@ -1,41 +1,23 @@
-local is_executable = require("utils.executable").is_executable
-
 ---@type LazySpec
 return {
   "nvim-neotest/neotest",
   specs = {
     {
-      "fredrikaverpil/neotest-golang",
+      "rcasia/neotest-java",
       specs = {
         {
           "nvim-neotest/neotest",
           opts = function(_, opts)
+            ---@type string
+            local jar = vim.fn.glob("$MASON/share/junit/junit-platform-console-standalone.jar", false, false)
+
             opts = opts or {}
             opts.adapters = opts.adapters or {}
 
-            ---@module 'neotest-golang'
-            ---@type NeotestGolangOptions
-            ---@diagnostic disable-next-line: missing-fields
             local adapter_opts = {
-              runner = is_executable("gotestsum") and "gotestsum" or "go",
-              env = {
-                CGO_ENABLED = "1",
-              },
-              go_test_args = {
-                "-v",
-                "-race",
-                "-count=1",
-                "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
-                "-tags=integration",
-              },
-              go_list_args = { "-tags=integration" },
-              dap_go_opts = {
-                delve = {
-                  build_flags = { "-tags=integration" },
-                },
-              },
+              junit_jar = jar ~= "" and jar or nil,
             }
-            table.insert(opts.adapters, require("neotest-golang")(adapter_opts))
+            table.insert(opts.adapters, require("neotest-java")(adapter_opts))
             return opts
           end,
         },
