ce36d18d809f71852f1c95f17e57caee76ddcf4e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-01-24 21:41:34 +0700

    fix(yamlls): no ignored when in compose file

--- after/lsp/yamlls.lua
+++ after/lsp/yamlls.lua
@@ -11,10 +11,10 @@ return {
     "yaml.openapi",
   },
   root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    if fname:match(".*compose.*%.ya?ml") then
-      return
-    end
+    -- local fname = vim.api.nvim_buf_get_name(bufnr)
+    -- if fname:match(".*compose.*%.ya?ml") then
+    --   return
+    -- end
     local parent = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr))
     if vim.endswith(parent, "/.github/workflows") or vim.endswith(parent, "/.forgejo/workflows") or vim.endswith(parent, "/.gitea/workflows") then
       return
