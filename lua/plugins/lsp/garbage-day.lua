fdfbdf418c30159e65b97da793d1f01ecb921882
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-03-13 21:12:57 +0700

    chore(garbage-day): keymap to lsp toggle

--- lua/plugins/lsp/garbage-day.lua
+++ lua/plugins/lsp/garbage-day.lua
@@ -2,6 +2,22 @@
 return {
   "Zeioth/garbage-day.nvim",
   event = "LspAttach",
+  keys = {
+    {
+      "<leader>lS",
+      function()
+        require("garbage-day.utils").stop_lsp()
+      end,
+      desc = "LSP | Stop All",
+    },
+    {
+      "<leader>l<C-s>",
+      function()
+        require("garbage-day.utils").start_lsp()
+      end,
+      desc = "LSP | Start Current",
+    },
+  },
   opts = {
     excluded_lsp_clients = {
       "copilot",
