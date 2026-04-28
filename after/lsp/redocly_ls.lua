6f904413c61e68f2265063029aee7fa3add20c95
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-04-28 14:21:58 +0700

    chore(redocly): yaml json?

--- after/lsp/redocly_ls.lua
+++ after/lsp/redocly_ls.lua
@@ -1,7 +1,8 @@
 ---@type vim.lsp.Config
 return {
   cmd = { "redocly-ls", "--stdio" },
-  filetypes = { "yaml.openapi" },
+  filetypes = { "yaml.openapi", "yaml", "json" },
   root_markers = { ".redocly.yaml", ".redocly.yml", "redocly.yaml", "redocly.yml" },
+  workspace_required = true,
   name = "redocly_ls",
 }
