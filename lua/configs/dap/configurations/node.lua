e95be5e3c9b7ee9d7383e4f5286af92282313cf8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-06-21 20:37:19 +0700

    refactor(dap): js to node for both js and ts

---
+++ lua/configs/dap/configurations/node.lua
@@ -0,0 +1,33 @@
+local dap = require("dap")
+local dap_utils = require("dap.utils")
+local pwa_node = require("configs.dap.adapters.pwa-node")
+
+local js_ts = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach to process",
+    processId = function()
+      return dap_utils.pick_process()
+    end,
+    cwd = "${workspaceFolder}",
+  },
+}
+
+local configurations = {
+  javascript = js_ts,
+  typescript = js_ts,
+}
+
+return function()
+  if pwa_node() then
+    dap.configurations = vim.tbl_deep_extend("force", dap.configurations or {}, configurations)
+  end
+end
--- lua/plugins/languages/typescript/nvim-dap.lua
+++ lua/plugins/languages/typescript/nvim-dap.lua
@@ -2,7 +2,7 @@
 return {
   "mfussenegger/nvim-dap",
   opts = function()
-    require("configs.dap.configurations.javascript")()
+    require("configs.dap.configurations.node")()
   end,
   optional = true,
 }
