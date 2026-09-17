6f059456dac541c958712ebef6b16118618526fe
kevin.t <kevin.t@itcgroup.io>
2026-09-17 15:09:08 +0700

    refactor(oil): map to -, remove some default opts

--- lua/plugins/others/oil.lua
+++ lua/plugins/others/oil.lua
@@ -3,36 +3,26 @@ return {
   "stevearc/oil.nvim",
   keys = {
     {
-      "<leader>fo",
+      "-",
       "<CMD>Oil --float<CR>",
       desc = "Oil | Toggle",
       silent = true,
     },
   },
   cmd = "Oil",
+  ---@module 'oil'
+  ---@type oil.Config
+  ---@diagnostic disable-next-line: missing-fields
   opts = {
-    -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-    delete_to_trash = false,
+    ---@diagnostic disable-next-line: missing-fields
     view_options = {
-      -- Show files and directories that start with "."
       show_hidden = true,
     },
-    -- Configuration for the floating window in oil.open_float
+    ---@diagnostic disable-next-line: missing-fields
     float = {
-      -- Padding around the floating window
       padding = 2,
       max_height = math.ceil(vim.o.lines * 0.8 - 4),
       max_width = math.ceil(vim.o.columns * 0.4),
-      border = "rounded",
-      win_options = {
-        winblend = 0,
-      },
-      -- This is the config that will be passed to nvim_open_win.
-      -- Change values here to customize the layout
-      override = function(conf)
-        return conf
-      end,
     },
-    use_default_keymaps = true,
   },
 }
